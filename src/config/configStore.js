f1b207626641f11e97f1337d3a4e6352d12fe9a9
19Edwin92 <b_a_s_e@naver.com>
2023-03-14 17:27:48 +0900

    devtools

--- src/config/configStore.js
+++ src/config/configStore.js
@@ -7,7 +7,15 @@ const store = configureStore({
     // todosT : thunk 로 제어하는 중앙상태관리소
     // todosQ : React-query 로 제어하는 중앙상태관리소
     todosT, todosQ
-  }
+  },
+  devTools: process.env.NODE_ENV === 'developmetns' ? false : true
+  // devTools: false;
+  // https://nodejs.dev/en/learn/nodejs-the-difference-between-development-and-production/
+  // yarn add redux-devtools-extension
+  // https://ui.toast.com/weekly-pick/ko_20191212
+  // development 환경에서만 redux devtool이 활성화 되도록 처리합니다.
+
+
 });
 
 export default store;
--- src/pages/TDMain.js
+++ src/pages/TDMain.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Thunk from '../components/Thunk'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import Reactquery from '../components/Reactquery'
-import { useSelector } from 'react-redux'
 
 const queryClient = new QueryClient()
 // https://sso-feeling.tistory.com/738 ////////////////////////////////////////////////
 // QueryClient() 는 하나의 컴포넌트만 감싸야 한다. 그렇지 않으면 동작하지 않는다. 
 
 function TDMain() {
+  // 하위 컴포넌트에서 값이 변경되었을 때 하위컴포넌트가 리렌더링되도록 
+  
   return (
     <div>
       <h1>Thunk와 React-query 비교하기</h1>  
